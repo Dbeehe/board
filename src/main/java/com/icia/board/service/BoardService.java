3737d96f8ae8ec36d393b01ad1f6380fcb112e75
Dbeehe <cjh123456a@naver.com>
2023-10-04 11:09:55 +0900

    게시판기능  2. 글목록

--- src/main/java/com/icia/board/service/BoardService.java
+++ src/main/java/com/icia/board/service/BoardService.java
@@ -6,6 +6,9 @@ import com.icia.board.repository.BoardRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class BoardService {
@@ -14,5 +17,14 @@ public class BoardService {
     public Long save(BoardDTO boardDTO) {
         BoardEntity boardEntity = BoardEntity.toSaveEntity(boardDTO);
         return boardRepository.save(boardEntity).getId();
+    }
+
+    public List<BoardDTO> findAll() {
+        List<BoardEntity> boardEntityList = boardRepository.findAll();
+        List<BoardDTO> boardDTOList = new ArrayList<>();
+        for (BoardEntity boardEntity : boardEntityList){
+            boardDTOList.add(BoardDTO.toDTO(boardEntity));
+        }
+        return boardDTOList;
     }
 }
